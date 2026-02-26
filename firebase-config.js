be07912b6b7eaf589344f8259f23bb47deb95118
aadelahmad64-ops <aadelahmad64@gmail.com>
2026-02-26 07:38:30 +0300

    Add Firebase configuration and initialization

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+apiKey: "AIzaSyBgU2e3nrFc6k741ghQdB7UqqwNYaEvATk",
+authDomain: "bra-alsalfa.firebaseapp.com",
+databaseURL: "https://bra-alsalfa-default-rtdb.europe-west1.firebasedatabase.app",
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
+const auth = firebase.auth();
+
+auth.signInAnonymously();
